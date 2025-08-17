f47df0a24761cddeab40e93747b6fc20b232b5b9
kl3inIT <dathip04@gmail.com>
2025-08-17 21:56:12 +0700

    init

---
+++ src/main/java/com/company/airbytedemo/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package com.company.airbytedemo.view.user;
+
+import com.company.airbytedemo.entity.User;
+import com.company.airbytedemo.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController(id = "User.list")
+@ViewDescriptor(path = "user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
